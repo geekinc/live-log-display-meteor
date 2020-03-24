2a5ce9025943f306d8ff43ffe616f3ceaf81647d
Ben Inkster <ben@geekinc.ca>
2020-03-23 20:24:56 -0600

    Epoch time

--- client/main.js
+++ client/main.js
@@ -2,7 +2,8 @@ import '../imports/startup/accounts-config.js';
 import '../imports/ui/body.js';
 
 Template.registerHelper('formatDate', function(date) {
-    return moment(date).format('MM/DD/YYYY HH:MM:SS');
+    // return moment(date).format('MM/DD/YYYY HH:MM:SS');
+    return moment(date).valueOf();
 });
 
 Template.registerHelper('today', function() {
